8212642cccc2228c1b9c9c3ee781b7840c96734f
Gagandeep Kang <gskang@bu.edu>
2019-10-23 22:27:41 -0400

    Added bootstrapvue  template for registration page

---
+++ src/components/Registration.vue
@@ -0,0 +1,105 @@
+<template>
+    <div class="container"> 
+        <div>
+            <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+            <b-form-group
+                id="input-group-1"
+                label="First Name':"
+                label-for="input-1"
+            >
+                <b-form-input
+                id="input-1"
+                v-model="form.firstName"
+                required
+                placeholder="Enter first name"
+                ></b-form-input>
+            </b-form-group>      
+            
+            <b-form-group
+                id="input-group-1"
+                label="Last Name:"
+                label-for="input-1"
+            >
+                <b-form-input
+                id="input-1"
+                v-model="form.lastName"
+                required
+                placeholder="Enter last name"
+                ></b-form-input>
+            </b-form-group>
+
+            <b-form-group
+                id="input-group-1"
+                label="Email address:"
+                label-for="input-1"
+            >
+                <b-form-input
+                id="input-1"
+                v-model="form.email"
+                type="email"
+                required
+                placeholder="Enter email"
+                ></b-form-input>
+            </b-form-group>
+
+            <b-form-group id="input-group-2" label="Your Password:" label-for="input-2">
+                <b-form-input
+                id="input-2"
+                v-model="form.password"
+                required
+                placeholder="Enter password"
+                ></b-form-input>
+            </b-form-group>
+
+
+            <b-form-group id="input-group-4">
+                <b-form-checkbox-group v-model="form.checked" id="checkboxes-4">
+                <b-form-checkbox value="me">Check me out</b-form-checkbox>
+                <b-form-checkbox value="that">Check that out</b-form-checkbox>
+                </b-form-checkbox-group>
+            </b-form-group>
+
+            <b-button type="submit" variant="primary">Submit</b-button>
+            <b-button type="reset" variant="danger">Reset</b-button>
+            </b-form>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        form: {
+            firstName: "", 
+            lastName: "", 
+            email: "",
+            password: "", 
+        }, 
+        show: true
+      }
+    },
+    methods: {
+      onSubmit(evt) {
+        evt.preventDefault()
+      },
+      onReset(evt) {
+        evt.preventDefault()
+        // Reset our form values
+        this.form.email = ''
+        this.form.name = ''
+        this.form.food = null
+        this.form.checked = []
+        // Trick to reset/clear native browser form validation state
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
