795e2bdaf29e1009670209920bdd87e16755d6a9
Minh Trinh <mtrinh@bu.edu>
2019-10-23 22:13:37 -0400

    New upload form

---
+++ src/components/Upload.vue
@@ -0,0 +1,42 @@
+import { BFormFile } from 'bootstrap-vue'
+Vue.component('b-form-file', BFormFile)
+
+<template>
+  <div class="container">
+    <!-- Styled -->
+    <b-form-file
+      id="file-large"
+      size="lg"
+      accept=".csv, .txt, .xlsm"
+      v-model="file"
+      :state="Boolean(file)"
+      placeholder="No file chosen"
+      ref="file-upload"
+    ></b-form-file>
+    <div class="mt-2">Selected file: {{ file ? file.name : '' }}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        file: null
+      }
+    },
+    methods: {
+      clearFiles() {
+        this.$refs['file-upload'].reset()
+      }
+    }
+  }
+</script>
+
+<style lang="css">
+.b-form-file {
+  text-align: left
+}
+.mt-2 {
+  text-align: left
+}
+</style>
---
+++ src/views/UploadScreen.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="uploadscreen"> 
+    <h1>Upload file here </h1>
+    <Upload />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Upload from '@/components/Upload.vue'
+
+export default {
+  name: 'uploadscreen',
+  components: {
+    Upload
+  }
+}
+</script>
