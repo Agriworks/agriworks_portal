48054525bb9e9f6a5d25bda22b88bc0abdce1320
Khoa Tran <kttran2320@gmail.com>
2019-10-30 21:21:51 -0400

    Initial Routing

--- src/router.js
+++ src/router.js
@@ -1,25 +1,84 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
-export default new Router({
-  mode: 'history',
+const router = new Router({
+  mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: () => import("./views/Login.vue")
+    },
+    {
+      path: "/register",
+      name: "registration",
+      component: () => import("./views/Registration.vue")
+    },
+    {
+      path: "/dashboard",
+      name: "dashboard",
+      component: () => import("./views/Dashboard.vue"),
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: "/admin",
+      name: "admin",
+      component: () => import("./views/Admin.vue"),
+      meta: {
+        requiresAuth: true,
+        is_admin: true
+      }
     }
   ]
-})
+});
+
+router.beforeEach((to, from, next) => {
+  //hard code for now
+  let auth = true;
+  let admin = true;
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (auth) {
+      if (to.matched.some(record => record.meta.is_admin)) {
+        //check to see if admin
+        if (admin) {
+          next();
+        }
+        // else{
+        //   alert user does not have admin privileges
+        // }
+      } else {
+        //authorize to dashboard if user is logged in but is not admin
+        next();
+      }
+    } else {
+      //redirect to login page if user is not authorized to view dashboard
+      next({
+        path: "/login",
+        query: { redirect: to.fullPath }
+      });
+    }
+  } else {
+    next();
+  }
+});
+export default router;
