e7f7f60c86cb3e0dbf150e35256289bb027e531d
Minh Trinh <mtrinh@bu.edu>
2019-11-06 19:05:14 -0500

    Added field selection

---
+++ src/components/FieldLink.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <multiselect v-model="value" :options="options" :multiple="true" placeholder="Select a field"></multiselect>
+  </div>
+</template>
+
+<script>
+  import Multiselect from 'vue-multiselect'
+
+  export default {
+    // OR register locally
+    components: { Multiselect },
+    data () {
+      return {
+        value: null,
+        options: ['Data-field-1', 'Data-field-2', 'Data-field-3']
+      }
+    }
+  }
+</script>
+
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -1,42 +1,99 @@
-import { BFormFile } from 'bootstrap-vue'
-Vue.component('b-form-file', BFormFile)
-
 <template>
-  <div class="container">
-    <!-- Styled -->
-    <b-form-file
-      id="file-large"
-      size="lg"
-      accept=".csv, .txt, .xlsm"
-      v-model="file"
-      :state="Boolean(file)"
-      placeholder="No file chosen"
-      ref="file-upload"
-    ></b-form-file>
-    <div class="mt-2">Selected file: {{ file ? file.name : '' }}</div>
+  <div class="table">
+      <b-card-group deck>
+        <b-card title="Meta Data" style="max-width: 50%;">
+          <div class="container">
+            <b-form inline>
+              <label class="sr-only" for="inline-form-input-meta1">Meta Data #1</label>
+              <b-input-group prepend="Field 1" class="mr-sm-5 mb-sm-4">
+                <b-input
+                  id="inline-form-input-meta1"
+                  placeholder="Meta Data #1"
+                ></b-input>
+              </b-input-group>
+              <label class="sr-only" for="inline-form-input-meta2">Meta Data #2</label>
+              <b-input-group prepend="Field 2" class="mb-sm-4">
+                <b-input
+                  id="inline-form-input-meta2"
+                  placeholder="Meta Data #2"
+                ></b-input>
+              </b-input-group>
+            </b-form>
+            <b-form inline>
+              <label class="sr-only" for="inline-form-input-meta3">Meta Data #3</label>
+              <b-input-group prepend="Field 3" class="mr-sm-5">
+                <b-input
+                  id="inline-form-input-meta3"
+                  placeholder="Meta Data #3"
+                ></b-input>
+              </b-input-group>
+              <label class="sr-only" for="inline-form-input-meta4">Meta Data #4</label>
+              <b-input-group prepend="Field 4">
+                <b-input
+                  id="inline-form-input-meta4"
+                  placeholder="Meta Data #4"
+                ></b-input>
+              </b-input-group>
+            </b-form>
+          </div>
+        </b-card>
+        <b-card title="Link Fields" style="max-width: 50%;">
+          <div style="margin-bottom: 20px;">
+            <b-card-text>Field 1</b-card-text>
+              <div class=fields_row>
+                <b-input
+                  id="inline-form-input-name"
+                  placeholder="Search Dataset"
+                  style="margin-right: 10px; height: 42px; max-width: 50%;"
+                ></b-input>
+                <FieldLink style="width: 50%;"/>
+              </div>
+          </div>
+          <div>
+            <b-card-text>Field 2</b-card-text>
+            <div class=fields_row>
+              <b-input
+                id="inline-form-input-name"
+                placeholder="Search Dataset"
+                style="margin-right: 10px; height: 42px; max-width: 50%;"
+              ></b-input>
+              <FieldLink style="width: 50%;"/>
+            </div>
+          </div>
+        </b-card>
+      </b-card-group>
+      <b-card-group deck style="padding-top: 15px;">
+        <b-card title="Selection" style="max-width: 50%;">
+          <div>
+            <b-form-file v-model="file" class="mt-3" plain></b-form-file>
+            <div class="mt-3">Selected file: {{ file ? file.name : '' }}</div>
+          </div>
+        </b-card>
+        <b-card title="Preparation" style="max-width: 50%;">
+          <b-card-text>
+            The following video will give you an explanation of how to prepare your data. Please note that all uploaded files will have either the .csv or .txt extension.
+          </b-card-text>
+        </b-card>
+      </b-card-group>
   </div>
 </template>
 
 <script>
+  import FieldLink from './FieldLink'
   export default {
-    data() {
-      return {
-        file: null
-      }
-    },
-    methods: {
-      clearFiles() {
-        this.$refs['file-upload'].reset()
-      }
-    }
+    components: { FieldLink }
   }
 </script>
 
 <style lang="css">
-.b-form-file {
-  text-align: left
+.table {
+  padding: 20px;
 }
-.mt-2 {
-  text-align: left
+.form-inline {
+  justify-content: center;
+}
+.fields_row {
+  display: flex;
+  flex-direction: row;
 }
 </style>
