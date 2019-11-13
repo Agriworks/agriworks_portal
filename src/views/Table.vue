2bb15158167278ffdbc8ff8cf03f90d89f1adf84
bjoernh2000 <bjoernh@bu.edu>
2019-11-12 23:26:06 -0500

    import from store.js, added TableView

--- src/views/Table.vue
+++ src/views/Table.vue
@@ -1,25 +1,17 @@
 <template>
   <div>
-    <b-table striped hover :items="items"></b-table>
+    <b-table striped hover :items="$store.state.items"></b-table>
   </div>
 </template>
 <script>
 import Vue from "vue";
 import BootstrapVue from "bootstrap-vue";
+// import Data from csv
 
 Vue.use(BootstrapVue);
   export default {
     name: "Table",
-    data() {
-      return {
-        items: [
-          { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-          { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-          { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-          { age: 38, first_name: 'Jami', last_name: 'Carney' }
-        ]
-      }
-    }
+    
   }
 </script>
 <style scoped>
