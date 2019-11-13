2c3580338fa2183b409b4d58f354848f69982825
bjoernh2000 <bjoernh@bu.edu>
2019-11-12 22:41:12 -0500

    hardcoded Table.vue

--- src/views/Table.vue
+++ src/views/Table.vue
@@ -1,95 +1,22 @@
-<link href="https://unpkg.com/bootstrap-table@1.15.5/dist/bootstrap-table.min.css" rel="stylesheet">
-
-<script src="https://cdn.jsdelivr.net/npm/vue"></script>
-<script src="https://unpkg.com/bootstrap-table@1.15.5/dist/bootstrap-table.min.js"></script>
-<script src="https://unpkg.com/bootstrap-table@1.15.5/dist/bootstrap-table-vue.min.js"></script>
 <template>
-    <div id="table">
-        <bootstrap-table :columns="columns" :data="data" :options="options"></bootstrap-table>
-</div>
+  <div>
+    <b-table striped hover v-bind:items="items"></b-table>
+  </div>
 </template>
-<script>
-export default {
-    name: "Table",
 
-}
-$(function() {
-    new Vue({
-      el: '#table',
-      components: {
-        'BootstrapTable': BootstrapTable
-      },
-      data: {
-        columns: [
-          {
-            field: 'state',
-            checkbox: true
-          },
-          {
-            title: 'Item ID',
-            field: 'id'
-          },
-          {
-            field: 'name',
-            title: 'Item Name'
-          },
-          {
-            field: 'price',
-            title: 'Item Price'
-          },
-          {
-            field: 'action',
-            title: 'Actions',
-            align: 'center',
-            formatter: function () {
-              return '<a href="javascript:" class="like"><i class="fa fa-star"></i></a>'
-            },
-            events: {
-              'click .like': function (e, value, row) {
-                alert(JSON.stringify(row))
-              }
-            }
-          }
-        ],
-        data: [
-          {
-            id: 0,
-            name: 'Item 0',
-            price: '$0'
-          },
-          {
-            id: 1,
-            name: 'Item 1',
-            price: '$1'
-          },
-          {
-            id: 2,
-            name: 'Item 2',
-            price: '$2'
-          },
-          {
-            id: 3,
-            name: 'Item 3',
-            price: '$3'
-          },
-          {
-            id: 4,
-            name: 'Item 4',
-            price: '$4'
-          },
-          {
-            id: 5,
-            name: 'Item 5',
-            price: '$5'
-          }
-        ],
-        options: {
-          search: true,
-          showColumns: true
-        }
+<script>
+  export default {
+    data() {
+      return {
+        items: [
+          { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+          { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+          { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+          { age: 38, first_name: 'Jami', last_name: 'Carney' }
+        ]
       }
-    })
-  })
+    }
+  }
 </script>
 <style scoped>
 
