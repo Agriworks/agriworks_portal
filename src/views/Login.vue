ae15c7e4bc9b3f28afc021cfdea022a033285f18
Khoa Tran <kttran2320@gmail.com>
2019-11-13 21:59:06 -0500

    fixed typo

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -54,7 +54,7 @@ export default {
     login() {
       this.$store
         .dispatch("retrieveSessionID", {
-          username: this.username,
+          username: this.email,
           password: this.password
         })
         .then(response => {
