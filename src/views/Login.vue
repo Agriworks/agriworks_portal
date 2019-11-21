f3f72d3a3540b3c68d684c0962bbe6701119e8e5
Pauli <mjqpauli@bu.edu>
2019-11-21 16:40:10 -0500

    added login styling

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+      <!-- <div class="col-md-1"></div> -->
+      <div class="col-md-6">
         <div class="shadow rounded">
           <div class="card-body">
             <h3 class="card-title text-center">Login</h3>
@@ -28,14 +29,34 @@
                 <label class="custom-control-label" for="customCheck1">Remember password</label>
               </div>
 
-              <div class="custom-control custom-checkbox mb-3">
+              <div class="custom-control custom-checkbox mb-3" align="center">
                 <button class="btn btn-lg btn-primary" type="submit">Login</button>
-                <router-link to="/registration" class="btn btn-link">Register</router-link>
+              </div>
+              <div class="custom-control custom-checkbox mb-3">
+                <router-link to="/registration" class="btn btn-link">Register</router-link> | <router-link to="/registration" class="btn btn-link">Forgot Password?</router-link>
               </div>
             </form>
           </div>
         </div>
       </div>
+      <div class="col-md-1"></div>
+      <div class="col-md-5">
+        
+          <div class="placeholder">
+            <img class="card-img center" src="../assets/Welcome.png" alt="Card image cap" align="middle">
+            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+            <a href="#!">
+              <div class="mask rgba-white-slight"></div>
+            </a>
+          </div>
+        
+      </div>
     </div>
   </div>
 </template>
+
+<style>
+  .col-md-6 {
+    padding: 1.5em 1.5em 1.5em 1.5em;
+  }
+</style>
