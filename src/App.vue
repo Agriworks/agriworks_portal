8a9a0a65f43ae5eae89c28aee2beb5ece14d0a36
Nguyen Quang <nddangquang@gmail.com>
2020-01-02 10:51:15 -0500

    Updated sidebar using Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
 <template>
-<v-app>
-  <div id="app" :class="[{'collapsed' : collapsed}]">
-    <div class="container">
-      <Sidebar v-on:collapsed="onCollapsed"/>
-      <div id="header">
-        <div id="header-left">
-          <Logo/>
-        </div>
-        <div id="header-right">
-          <!-- Log in and sign in buttons. Display only if not logged in !--> 
-        </div>
-      </div>
-      <!-- <Push>
-        <a id="logo">
-            <Logo/>
-        </a>
-        <router-link v-for="route in routes" :key="route.path" :to="route.path">
-            {{route.name}}  
-        </router-link>
-      </Push> -->
-        <main id="page-wrap">
+  <v-app>
+    <Sidebar />
+    <div  on:click="closeSidebar">
+      <v-content>
+          <div id="header">
+            <div id="header-left">
+              <Logo/>
+            </div>
+            <div id="header-right">
+            <!-- Log in and sign in buttons. Display only if not logged in !--> 
+            </div>
+          </div>
           <router-view />
-        </main>
-      </div>
-    </div>  
-    </v-app>
+      </v-content>
+    </div>
+  </v-app>
 </template>
 
 <script>
@@ -39,38 +29,20 @@ export default {
   data() {
     return {
       routes: this.$router.options.routes,
-      collapsed: true
+      mini: true
     };
   },
   methods: {
-    onCollapsed(collapsed) {
-      this.collapsed = collapsed
-    },
     getYear(){
       var d = new Date();
       const currentYear = d.getFullYear();
       return currentYear;
-    }
+    },
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  background-color: white;
-}
-
-#app.collapsed {
-  padding-left: 50px;
-}
-
-.container-fluid {
-  margin-top: 1rem;
-}
 
 #header {
   display: flex;
