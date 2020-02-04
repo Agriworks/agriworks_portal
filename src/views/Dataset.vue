5c635346c5693779197769c07000f22d6de507d4
Cody Clark-Singh <cody@sigres.com>
2020-02-04 04:24:02 -0500

    updated positioning of metadata card

--- src/views/Dataset.vue
+++ src/views/Dataset.vue
@@ -1,31 +1,37 @@
 <template>
-<div class="row">
-  <div class="col-md-12">
-  <h5> {{dataset.type}} </h5>
-  <h1> {{ dataset.name }} </h1>
-  <h3 v-if="dataset.tags"> Tags: {{ dataset.tags }} </h3>
-  <p> By {{ dataset.author }} </p>
-  <v-app id="app">
-  <v-container ma-0 pa-0>
-    <v-layout>
-      <v-flex xs12 sm6>
-        <v-card hover>
-
-          <v-card-title>
-            <h2>Metadata</h2>
-          </v-card-title>
+  <div>
+    <div class="row">
+      <div class="col-sm-6">
+        <h5> {{dataset.type}} </h5>
+        <h1> {{ dataset.name }} </h1>
+        <h3 v-if="dataset.tags"> Tags: {{ dataset.tags }} </h3>
+        <p> By {{ dataset.author }} </p>
+      </div>
+      <div class="col-sm-6">
+        <v-app id="app">
+          <v-container>
+            <v-layout>
+              <v-flex xs12 sm6 offset-xs8>
+                <v-card hover>
+                  <v-card-title>
+                    <h2>Metadata</h2>
+                  </v-card-title>
           
-          <v-card-text>
-            <p>{{dataset.meta}}</p>
-          </v-card-text>
+                  <v-card-text>
+                    <p> Type: {{dataset.type}} </p>
+                    <p> Name: {{ dataset.name }} </p>
+                  </v-card-text>
           
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
-</v-app>
-  <DataTable :headers="dataset.headers" :data="dataset.data"/>
-  </div>
+                  </v-card>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-app>
+      </div>
+    </div>
+    <div class="row">
+      <DataTable :headers="dataset.headers" :data="dataset.data"/>
+    </div>
   </div>
 </template>
 
