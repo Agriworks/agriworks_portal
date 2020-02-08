547b963a5db1e4ef3a41f4203f553873ee4b0524
Khoa Tran <kttran2320@gmail.com>
2020-02-08 18:01:47 -0500

    added loading for when email sends

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -5,9 +5,10 @@
         <div class="card card-signin my-5">
           <div class="card-body">
             <h3 class="card-title text-center">Reset Password</h3>
-            <h6
-              class="card-title text-center"
-            >Enter your user account's email address and we will send you a password reset link.</h6>
+            <h6 class="card-title text-center">
+              Enter your user account's email address and we will send you a
+              password reset link.
+            </h6>
             <form class="form-signin" @submit.prevent="submit">
               <div class="form-label-group">
                 <input
@@ -20,17 +21,26 @@
                   autofocus
                 />
               </div>
-              <div class="custom-control custom-checkbox mb-3 text-center" style="padding:0">
+              <div
+                class="custom-control custom-checkbox mb-3 text-center"
+                style="padding:0"
+              >
                 <v-btn
                   color="success"
                   :outlined="true"
                   type="submit"
                   style="margin:0"
-                >Reset Password</v-btn>
+                  :loading="loading"
+                  >Reset Password</v-btn
+                >
               </div>
               <div class="text-center">
-                <v-btn to="/login" color="success" :text="true">Return to login</v-btn>|
-                <v-btn to="/registration" color="success" :text="true">Create new account</v-btn>
+                <v-btn to="/login" color="success" :text="true"
+                  >Return to login</v-btn
+                >|
+                <v-btn to="/registration" color="success" :text="true"
+                  >Create new account</v-btn
+                >
               </div>
             </form>
           </div>
@@ -43,12 +53,19 @@
 <script>
 import { post } from "../requests";
 export default {
+  data() {
+    return {
+      loading: false
+    };
+  },
   methods: {
     submit() {
+      this.loading = true;
       post("/auth/forgot-password", {
         email: document.getElementById("inputText").value
       })
         .then(res => {
+          this.loading = false;
           this.$store.commit("setSnackbar", {
             message: res.data.message,
             show: true,
@@ -56,6 +73,7 @@ export default {
           });
         })
         .catch(err => {
+          this.loading = false;
           this.$store.commit("setSnackbar", {
             message: err.response.data.message,
             show: true,
