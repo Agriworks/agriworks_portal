28287c093b568670f3b03062773e3cddea8a163c
Conor Walsh <cewalsh@bu.edu>
2020-02-24 13:13:41 -0500

    Made the chart picture change when dark mode is enabled.

--- src/views/DatasetCard.vue
+++ src/views/DatasetCard.vue
@@ -7,7 +7,8 @@
                 <v-list-item-subtitle> By {{ dataset.author }} </v-list-item-subtitle>
             </v-list-item-content>
             <v-list-item-avatar size="80" tile>
-                <v-img src="https://cdn4.iconfinder.com/data/icons/business-app-enjoy/74/2-512.png"/>
+                 <v-img v-if="!$vuetify.theme.dark" src="https://cdn4.iconfinder.com/data/icons/business-app-enjoy/74/2-512.png"/> 
+                <v-img v-if="$vuetify.theme.dark" src="https://www.ndctech.net/media/2018/08/icon-build-future.png"/>
             </v-list-item-avatar>
         </v-list-item>
         <v-card-actions>
