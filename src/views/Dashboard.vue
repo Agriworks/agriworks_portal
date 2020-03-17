6c216ae2fd1dacaa7b82809f6c077fa92a869a72
Gagandeep Kang <gskang@bu.edu>
2020-03-16 20:36:52 -0400

    added title and vue button

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,139 +1,157 @@
 <template>
-  <div class="accordian" id="types">
-    <v-card class="card">
-      <div class="card-header" id="popularHeader">
-        <h2 class="mb-0">
-          <button
-            class="btn btn-link"
-            type="button"
-            data-toggle="collapse"
-            data-target="#popularContent"
-            aria-expanded="true"
-            aria-controls="popularContent"
-          >Popular</button>
-        </h2>
-      </div>
+  <div>
+    <h1 class="datasetTitle">Trending Datasets</h1>
+    <div class="accordian" id="types">
+      <v-card class="card">
+        <div class="card-header" id="popularHeader">
+          <h2 class="mb-0">
+            <v-btn
+              text
+              type="button"
+              data-toggle="collapse"
+              data-target="#popularContent"
+              aria-expanded="true"
+              aria-controls="popularContent"
+            >Popular</v-btn>
+          </h2>
+        </div>
 
-      <div
-        id="popularContent"
-        class="collapse show"
-        aria-labelledby="popularHeader"
-        data-parent="#types"
-      >
-        <div class="card-body">
-          <div id="popularCarousel" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
-              <div
-                class="carousel-item"
-                v-for="(dataset, index) in popularDatasets"
-                v-bind:key="dataset.id"
-                v-bind:class="{ active: index == 0}"
-              >
-                <DatasetCard v-bind:dataset="dataset" />
+        <div
+          id="popularContent"
+          class="collapse show"
+          aria-labelledby="popularHeader"
+          data-parent="#types"
+        >
+          <div class="card-body">
+            <div id="popularCarousel" class="carousel slide" data-ride="carousel">
+              <div class="carousel-inner">
+                <div
+                  class="carousel-item"
+                  v-for="(dataset, index) in popularDatasets"
+                  v-bind:key="dataset.id"
+                  v-bind:class="{ active: index == 0}"
+                >
+                  <DatasetCard v-bind:dataset="dataset" />
+                </div>
               </div>
+              <a
+                class="carousel-control-prev"
+                href="#popularCarousel"
+                role="button"
+                data-slide="prev"
+              >
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a
+                class="carousel-control-next"
+                href="#popularCarousel"
+                role="button"
+                data-slide="next"
+              >
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
             </div>
-            <a
-              class="carousel-control-prev"
-              href="#popularCarousel"
-              role="button"
-              data-slide="prev"
-            >
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a
-              class="carousel-control-next"
-              href="#popularCarousel"
-              role="button"
-              data-slide="next"
-            >
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
           </div>
         </div>
-      </div>
-    </v-card>
+      </v-card>
 
-    <v-card class="card">
-      <div class="card-header" id="headerRecent">
-        <h2 class="mb-0">
-          <button
-            class="btn btn-link"
-            type="button"
-            data-toggle="collapse"
-            data-target="#recentContent"
-            aria-expanded="false"
-            aria-controls="recentContent"
-          >Recent</button>
-        </h2>
-      </div>
+      <v-card class="card">
+        <div class="card-header" id="headerRecent">
+          <h2 class="mb-0">
+            <v-btn
+              text
+              type="button"
+              data-toggle="collapse"
+              data-target="#recentContent"
+              aria-expanded="false"
+              aria-controls="recentContent"
+            >Recent</v-btn>
+          </h2>
+        </div>
 
-      <div id="recentContent" class="collapse" aria-labelledby="headerRecent" data-parent="#types">
-        <div class="card-body">
-          <div id="recentCarousel" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
-              <div
-                class="carousel-item"
-                v-for="(dataset, index) in recentDatasets"
-                v-bind:key="dataset.id"
-                v-bind:class="{ active: index == 0}"
-              >
-                <DatasetCard v-bind:dataset="dataset" />
+        <div
+          id="recentContent"
+          class="collapse"
+          aria-labelledby="headerRecent"
+          data-parent="#types"
+        >
+          <div class="card-body">
+            <div id="recentCarousel" class="carousel slide" data-ride="carousel">
+              <div class="carousel-inner">
+                <div
+                  class="carousel-item"
+                  v-for="(dataset, index) in recentDatasets"
+                  v-bind:key="dataset.id"
+                  v-bind:class="{ active: index == 0}"
+                >
+                  <DatasetCard v-bind:dataset="dataset" />
+                </div>
               </div>
+              <a
+                class="carousel-control-prev"
+                href="#recentCarousel"
+                role="button"
+                data-slide="prev"
+              >
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a
+                class="carousel-control-next"
+                href="#recentCarousel"
+                role="button"
+                data-slide="next"
+              >
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
             </div>
-            <a class="carousel-control-prev" href="#recentCarousel" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#recentCarousel" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
           </div>
         </div>
-      </div>
-    </v-card>
+      </v-card>
 
-    <v-card class="card">
-      <div class="card-header" id="headerNew">
-        <h2 class="mb-0">
-          <button
-            class="btn btn-link"
-            type="button"
-            data-toggle="collapse"
-            data-target="#newContent"
-            aria-expanded="false"
-            aria-controls="newContent"
-          >New</button>
-        </h2>
-      </div>
+      <v-card class="card">
+        <div class="card-header" id="headerNew">
+          <h2 class="mb-0">
+            <v-btn
+              text
+              type="button"
+              data-toggle="collapse"
+              data-target="#newContent"
+              aria-expanded="false"
+              aria-controls="newContent"
+            >New</v-btn>
+          </h2>
+        </div>
 
-      <div id="newContent" class="collapse" aria-labelledby="headerNew" data-parent="#types">
-        <div class="card-body">
-          <div id="newCarousel" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
-              <div
-                class="carousel-item"
-                v-for="(dataset, index) in newDatasets"
-                v-bind:key="dataset.id"
-                v-bind:class="{ active: index == 0}"
-              >
-                <DatasetCard v-bind:dataset="dataset" />
+        <div id="newContent" class="collapse" aria-labelledby="headerNew" data-parent="#types">
+          <div class="card-body">
+            <div id="newCarousel" class="carousel slide" data-ride="carousel">
+              <div class="carousel-inner">
+                <div
+                  class="carousel-item"
+                  v-for="(dataset, index) in newDatasets"
+                  v-bind:key="dataset.id"
+                  v-bind:class="{ active: index == 0}"
+                >
+                  <DatasetCard v-bind:dataset="dataset" />
+                </div>
               </div>
+              <a class="carousel-control-prev" href="#newCarousel" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#newCarousel" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
             </div>
-            <a class="carousel-control-prev" href="#newCarousel" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#newCarousel" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
           </div>
         </div>
-      </div>
-    </v-card>
+      </v-card>
+    </div>
   </div>
 </template>
 
@@ -193,5 +211,10 @@ export default {
 
 .carousel-inner {
   padding-left: 350px;
+}
+
+.datasetTitle {
+  margin-top: 20px;
+  margin-bottom: 20px;
 }
 </style>
