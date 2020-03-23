32dc96ea3a8dd00be7df82b7f3234e56f08be859
Khoa Tran <kttran2320@gmail.com>
2020-03-22 20:43:19 -0400

    made all dataset card buttons have outlined variant instead of text

--- src/views/DatasetCard.vue
+++ src/views/DatasetCard.vue
@@ -12,23 +12,23 @@
       </v-list-item-avatar>
     </v-list-item>
     <v-card-actions>
-       <v-btn class="linkButton" router-link :to="datasetLink" text color="var(--v-primary-base)"> Open </v-btn>  
-      <v-btn class="linkButton" v-if="manage == true" router-link :to="datasetLink" text color="orange">
+       <v-btn class="linkButton"  router-link :to="datasetLink" outlined color="var(--v-primary-base)"> Open </v-btn>  
+      <v-btn class="linkButton"  outlined v-if="manage == true" router-link :to="datasetLink"  color="orange">
         Edit
       </v-btn>
       <template>
         <v-row justify="center">
           <v-dialog v-model="dialog" persistent max-width="290">
             <template v-slot:activator="{ on }"> 
-              <v-btn v-if="manage == true" text color="error" v-on="on">Delete</v-btn>
+              <v-btn outlined v-if="manage == true"  color="error" v-on="on">Delete</v-btn>
             </template>
             <v-card>
               <v-card-title class="headline">Delete dataset?</v-card-title>
               <v-card-text>Once you delete this dataset you will not be able to revert these changes.</v-card-text>
               <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
-                <v-btn color="error" text @click="deleteDataset()">Delete</v-btn>
+                <v-btn outlined color="blue darken-1"  @click="dialog = false">Close</v-btn>
+                <v-btn outtlined color="error" @click="deleteDataset()">Delete</v-btn>
               </v-card-actions>
             </v-card>
           </v-dialog>
