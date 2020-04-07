3407fe6b96d0e3bf62b301be84af9d89b4924ff8
Gagandeep Kang <gskang@bu.edu>
2020-04-07 00:29:07 -0400

    fixed open dataset null error

--- src/views/Dataset.vue
+++ src/views/Dataset.vue
@@ -1,44 +1,65 @@
 <template>
-<div id="datasetContainer">
-    <div class="row">
-      <div class="d-flex justify-content-center col-sm-6" id="datasetInfoContainer">
-        <h5> {{dataset.type}} </h5>
-        <h1> {{ dataset.name }} </h1>
-        <div v-if="hideTags">
-          <div v-if="dataset.tags.length < 1">
-            <p>No tags</p>
+  <div id="datasetContainer">
+    <div v-if="dataset">
+      <div class="row">
+        <div class="d-flex justify-content-center col-sm-6" id="datasetInfoContainer">
+          <h5>{{dataset.type}}</h5>
+          <h1>{{ dataset.name }}</h1>
+          <div v-if="hideTags">
+            <div v-if="dataset">
+              <div v-if="dataset.tags.length < 1">
+                <p>No tags</p>
+              </div>
+            </div>
+            <div v-else>
+              <p
+                @click="changeTagStatus()"
+                style="color: #96D34A; text-decoration: underline;"
+              >Show tags +</p>
+            </div>
           </div>
           <div v-else>
-            <p @click="changeTagStatus()" style="color: #96D34A; text-decoration: underline;">Show tags +</p>
+            <p
+              @click="changeTagStatus()"
+              style="color: #96D34A; text-decoration: underline;"
+            >Hide tags -</p>
+            <div>
+              <span>
+                <v-chip
+                  v-for="tag in dataset.tags"
+                  v-bind:key="tag"
+                  color="#96D34A"
+                  style="margin-left: 5px;margin-bottom: 5px;"
+                >{{ tag }}</v-chip>
+              </span>
+            </div>
           </div>
-        </div>
-        <div v-else>
-          <p @click="changeTagStatus()" style="color: #96D34A; text-decoration: underline;">Hide tags -</p>
-          <div>
-            <span ><v-chip 
-              v-for="tag in dataset.tags" 
-              v-bind:key="tag"
-              color="#96D34A" 
-              style="margin-left: 5px;margin-bottom: 5px;">{{ tag }}
-              </v-chip>
-            </span>
-          </div>
-        </div>
-        <p> By {{ dataset.author }} </p>
-      <div class="row">
-        <div class="col-md-6">
-          <v-btn small dark color="#4caf50" id="downloadButton" style="margin-right:0.5rem;" @click="downloadDataset()">
-            <v-icon small>mdi-arrow-down-circle-outline</v-icon>Download
-          </v-btn>
-          <!--
+          <p>By {{ dataset.author }}</p>
+          <div class="row">
+            <div class="col-md-6">
+              <v-btn
+                small
+                dark
+                color="#4caf50"
+                id="downloadButton"
+                style="margin-right:0.5rem;"
+                @click="downloadDataset()"
+              >
+                <v-icon small>mdi-arrow-down-circle-outline</v-icon>Download
+              </v-btn>
+              <!--
           <v-btn small dark color="purple">
             <v-icon small>mdi-graph-outline</v-icon> Visualize
           </v-btn>
-          !-->
+              !-->
+            </div>
+          </div>
         </div>
-      </div> 
-      </div>
-      <div class="col-sm-6" v-if="Object.keys(this.dataset.legend).length > 0" id="metadataContainer">
+        <div
+          class="col-sm-6"
+          v-if="Object.keys(this.dataset.legend).length > 0"
+          id="metadataContainer"
+        >
           <v-container>
             <v-layout>
               <v-flex>
@@ -48,21 +69,31 @@
                   </v-card-title>
                   <v-card-text>
                     <div v-for="(value, name) in dataset.legend" :key="value">
-                      <strong> {{ name }} </strong> : {{ value }}
+                      <strong>{{ name }}</strong>
+                      : {{ value }}
                     </div>
                   </v-card-text>
-          
                 </v-card>
               </v-flex>
             </v-layout>
           </v-container>
+        </div>
       </div>
-    </div>
-    <div class="row">
-       <DataTable :headers="dataset.headers" :data="data" :isLoading="tableIsLoading" id="datatable"/>
-      <div class="row" v-if="additionalDataObjectsLoading" id="SubsequentDataObjectLoadingIndicator">
-        <p> Loading data ... </p>
-        <LoadingIndicator/>
+      <div class="row">
+        <DataTable
+          :headers="dataset.headers"
+          :data="data"
+          :isLoading="tableIsLoading"
+          id="datatable"
+        />
+        <div
+          class="row"
+          v-if="additionalDataObjectsLoading"
+          id="SubsequentDataObjectLoadingIndicator"
+        >
+          <p>Loading data ...</p>
+          <LoadingIndicator />
+        </div>
       </div>
     </div>
   </div>
@@ -73,37 +104,39 @@
 <script>
 import DataTable from "../components/DataTable";
 import LoadingIndicator from "../components/LoadingIndicator";
-import api from '../api';
-import notify from '../utilities/notify';
-import { colors } from '../utilities/branding';
+import api from "../api";
+import notify from "../utilities/notify";
+import { colors } from "../utilities/branding";
 import $ from "jquery";
 
 export default {
-    name: "Dataset", 
-    components: {
-      DataTable,
-      LoadingIndicator,
-    },
-    data(){
-      return {
-        hideTags : true,
-        dataset: null,
-        data: [],
-        cacheId: null,
-        tableIsLoading: true,
-        additionalDataObjectsLoading: false
-      }
-    },
-    created(){
-      api.fetchDatasetInfo(this.$route.params.id)
-      .then((response) => {
+  name: "Dataset",
+  components: {
+    DataTable,
+    LoadingIndicator
+  },
+  data() {
+    return {
+      hideTags: true,
+      dataset: null,
+      data: [],
+      cacheId: null,
+      tableIsLoading: true,
+      additionalDataObjectsLoading: false
+    };
+  },
+  created() {
+    api
+      .fetchDatasetInfo(this.$route.params.id)
+      .then(response => {
         this.dataset = response.data;
       })
       .catch(() => {
         notify("Error fetching dataset metadata.", colors.red);
       });
-      api.fetchPrimaryDatasetObjects(this.$route.params.id)
-      .then((response) => {
+    api
+      .fetchPrimaryDatasetObjects(this.$route.params.id)
+      .then(response => {
         this.data = response.data.datasetObjects;
         this.tableIsLoading = false;
         if (response.data.cacheId) {
@@ -113,12 +146,13 @@ export default {
       .catch(() => {
         notify("Error fetching dataset objects.", colors.red);
       });
-      $(window).scroll(() => {
-        if ($(window).scrollTop() + $(window).height() == $(document).height()) {
-          if (this.cacheId && !this.additionalDataObjectsLoading) {
-            this.additionalDataObjectsLoading = true;
-            api.fetchSubsequentDatasetObjects(this.cacheId)
-            .then((response) => {
+    $(window).scroll(() => {
+      if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+        if (this.cacheId && !this.additionalDataObjectsLoading) {
+          this.additionalDataObjectsLoading = true;
+          api
+            .fetchSubsequentDatasetObjects(this.cacheId)
+            .then(response => {
               this.data = this.data.concat(response.data.datasetObjects);
               this.additionalDataObjectsLoading = false;
               if (response.data.cacheId) {
@@ -127,53 +161,53 @@ export default {
                 this.cacheId = null;
               }
             })
-           .catch(() => {
-             notify("Error fetching subsequent dataset objects.", colors.red);
-           })
-          }
+            .catch(() => {
+              notify("Error fetching subsequent dataset objects.", colors.red);
+            });
         }
-      });
-    },
-    destroyed() {
-      if (this.cacheId) {
-        api.evictDatasetFromCache(this.cacheId)
-        .catch(() => {
-          notify("Error evicting dataset from cache.", colors.red);
-        })
       }
-      this.cacheId = null;
-      this.dataset = null;
-      this.data = null;
+    });
+  },
+  destroyed() {
+    if (this.cacheId) {
+      api.evictDatasetFromCache(this.cacheId).catch(() => {
+        notify("Error evicting dataset from cache.", colors.red);
+      });
+    }
+    this.cacheId = null;
+    this.dataset = null;
+    this.data = null;
+  },
+  methods: {
+    changeTagStatus() {
+      this.hideTags = !this.hideTags;
     },
-    methods: {
-      changeTagStatus() {
-        this.hideTags = !this.hideTags;
-      },
-      downloadDataset() {
-        api.downloadDataset(this.$route.params.id)
+    downloadDataset() {
+      api
+        .downloadDataset(this.$route.params.id)
         .then(response => {
           const fileURL = window.URL.createObjectURL(new Blob([response.data]));
-          let fileLink = document.createElement('a');
-          const fileName = this.$route.params.id + '.csv';
+          let fileLink = document.createElement("a");
+          const fileName = this.$route.params.id + ".csv";
 
           fileLink.href = fileURL;
-          fileLink.setAttribute('download', fileName);
+          fileLink.setAttribute("download", fileName);
           document.body.appendChild(fileLink);
 
           fileLink.click();
         })
         .catch(() => {
-          notify("Unable to download file. Please try again later.", colors.red)
-        })
-      },
+          notify(
+            "Unable to download file. Please try again later.",
+            colors.red
+          );
+        });
     }
-}
-
-
+  }
+};
 </script>
 
 <style scoped>
-
 #datatable {
   width: 100%;
   border: 2px solid #267a35de;
@@ -181,10 +215,10 @@ export default {
 }
 
 #datasetInfoContainer {
-  flex-direction: column
+  flex-direction: column;
 }
 
-#metadataCard{
+#metadataCard {
   border: 1px solid #a2e510;
 }
 
