d08f4b68469c10f33e7f9091850f3191f9ebab23
Manuja DeSilva <mdesilva@bu.edu>
2020-04-08 17:27:57 -0400

    remove comment

--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -152,7 +152,6 @@ export default {
         organization: this.inputOrganization,
         location: this.inputLocation,
         userType: this.selectedType.type
-        //, comfirmPassword: document.getElementByID("comfirmPassword").value (needs to be done in backend)
       })
         .then(res => {
           this.$router.push("login");
