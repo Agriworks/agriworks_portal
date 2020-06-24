ddcd3e9abb07da17652be31983304434e20a2801
Cody Clark-Singh <cody@sigres.com>
2020-06-24 00:00:33 -0400

    can display data from earthquake dataset as heatmap but is buggy

--- src/components/HeatMap.vue
+++ src/components/HeatMap.vue
@@ -11,41 +11,78 @@ export default {
   props: ['data'],
   mounted() {
     // lazy load the required ArcGIS API for JavaScript modules and CSS
-    loadModules(['esri/Map', 'esri/views/MapView', 'esri/Graphic', 'esri/layers/GraphicsLayer'], { css: true })
-    .then(([Map, MapView, Graphic, GraphicsLayer]) => {
+    loadModules(['esri/Map', 'esri/views/MapView', 'esri/Graphic', 'esri/layers/FeatureLayer'], { css: true })
+    .then(([Map, MapView, Graphic, FeatureLayer]) => {
 
       // The heatmap renderer assigns each pixel in the view with
       // an intensity value. The ratio of that intensity value
       // to the maxPixel intensity is used to assign a color
       // from the continuous color ramp in the colorStops property
-      const polyline = {
-        type: "polyline",  // autocasts as new Polyline()
-          paths: [
-            [-111.30, 52.68],
-            [-98, 49.5],
-            [-93.94, 29.89]
-          ]
+
+      var graphics = [];
+
+      for (var key in this.data) {
+        var item = this.data[key]
+        var pointGraphic = new Graphic({
+          attributes: {
+            ObjectId: item.place,
+            name: item.place
+          },
+          geometry: {
+            type: "point",                     // autocasts as new Point()
+            longitude: item.longitude,
+            latitude: item.latitude
+          },
+          symbol: {
+            type: "simple-marker",             // autocasts as new SimpleMarkerSymbol()
+            color: [ 226, 119, 40 ],
+            outline: {                         // autocasts as SimpleLineSymbol()
+              color: [ 255, 255, 255 ],
+              width: 2
+            }
+          },
+          popupTemplate: {                     // autocasts as new PopupTemplate()
+            title: item.type,
+            content: [{
+              type: "fields",
+              fieldInfos: [
+                {
+                  fieldName: "name",
+                  label: "Place",
+                  visible: true
+                }
+              ]
+            }]
+          },
+        });
+        graphics.push(pointGraphic);
+      }
+
+      const renderer = {
+        type: "heatmap",
+        colorStops: [
+          { color: "rgba(63, 40, 102, 0)", ratio: 0 },
+          { color: "#472b77", ratio: 0.083 },
+          { color: "#4e2d87", ratio: 0.166 },
+          { color: "#563098", ratio: 0.249 },
+          { color: "#5d32a8", ratio: 0.332 },
+          { color: "#6735be", ratio: 0.415 },
+          { color: "#7139d4", ratio: 0.498 },
+          { color: "#7b3ce9", ratio: 0.581 },
+          { color: "#853fff", ratio: 0.664 },
+          { color: "#a46fbf", ratio: 0.747 },
+          { color: "#c29f80", ratio: 0.83 },
+          { color: "#e0cf40", ratio: 0.913 },
+          { color: "#ffff00", ratio: 1 }
+        ],
+        maxPixelIntensity: 25,
+        minPixelIntensity: 0
       };
 
-      const polylineSymbol = {
-        type: "simple-line",  // autocasts as SimpleLineSymbol()
-        color: [226, 119, 40],
-        width: 4
-      };
-
-      const polylineAtt = {
-        Name: "Keystone Pipeline",
-        Owner: "TransCanada"
-      };
-
-      const polylineGraphic = new Graphic({
-        geometry: polyline,
-        symbol: polylineSymbol,
-        attributes: polylineAtt
-      });
-
-      const layer = new GraphicsLayer({
-        graphics: [polylineGraphic]
+      const layer = new FeatureLayer({
+        source: graphics,
+        renderer: renderer,
+        objectIdField: "ObjectID"
       });
 
       const map = new Map({
