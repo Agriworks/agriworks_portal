4a7f651640308fdd001567edd1315af80eae9236
Conor Walsh <cewalsh@bu.edu>
2020-10-10 13:23:49 -0400

    Made some changes

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -40,8 +40,53 @@ export default {
               features: new GeoJSON().readFeatures(geojsonObject),
             });
 
+            function getColor(d) {
+              return d > 1000 ? '#800026' :
+                    d > 500  ? '#BD0026' :
+                    d > 200  ? '#E31A1C' :
+                    d > 100  ? '#FC4E2A' :
+                    d > 50   ? '#FD8D3C' :
+                    d > 20   ? '#FEB24C' :
+                    d > 10   ? '#FED976' :
+                                '#FFEDA0';
+            }
+
+            function hexToRgb(hex) {
+              var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+              return result ? {
+                r: parseInt(result[1], 16),
+                g: parseInt(result[2], 16),
+                b: parseInt(result[3], 16)
+              } : null;
+            }
+
+            var getStyle = function (feature, resolution) {
+                if (feature.get('density') < 10) {
+                    return new Style({
+                        fill: new Fill({
+                            color: [255, 0, 0, 0.2] // semi-transparent red
+                        })
+                    });
+                }
+                else {
+                    return new Style({
+                        stroke: new Stroke({
+                          color: 'white',
+                          lineDash: [1],
+                          width: 1
+                        }),
+                        fill: new Fill({
+                            color: [hexToRgb(getColor(feature.get('density'))).r, hexToRgb(getColor(feature.get('density'))).g, hexToRgb(getColor(feature.get('density'))).b, 0.2]
+                        })
+                    }); 
+                }
+            };
+
             var vector = new VectorLayer({
-              source: vectorSource
+              source: vectorSource,
+              style: function (feature, resolution) {
+                return getStyle(feature, resolution);
+                }
             });
 
             var raster = new TileLayer({
