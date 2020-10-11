b580abd4860028cc2db0d2a99872137f21adf1f6
Conor <cewalsh@bu.edu>
2020-10-11 18:18:54 -0400

    When click on feature it highlights feature and zooms in on it

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -19,7 +19,10 @@ import {OSM, Vector as VectorSource} from 'ol/source';
 
 import Feature from 'ol/Feature';
 import GeoJSON from 'ol/format/GeoJSON';
-import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style';
+import {Circle as CircleStyle, Fill, Stroke, Style, Text} from 'ol/style';
+import Select from 'ol/interaction/Select';
+import {altKeyOnly, click, pointerMove} from 'ol/events/condition';
+
 export default {
   props: ['dataset', 'hasMapData'],
   watch: {
@@ -52,28 +55,48 @@ export default {
               features: new GeoJSON().readFeatures(geojsonObject),
             });
 
-            var getStyle = function (feature, resolution) {
+            var getStyle = function (feature, resolution, highlighted) {
                 var color = feature.get('color')
+                var width = 1
+                if(highlighted){
+                    width = 4
+                }
                 if(color){
-
                   return new Style({
                         stroke: new Stroke({
                           color: 'white',
                           lineDash: [1],
-                          width: 1
+                          width: width,
                         }),
                         fill: new Fill({
-                            
+                          
                             color: [color[0], color[1], color[2],  0.4]
-                        })
+                        }),
+                        text: new Text({
+                          text: feature.get("NAME"),
+                          font: '12px Calibri,sans-serif',
+                          fill: new Fill({
+                            color: '#000',
+                          }),
+                          stroke: new Stroke({
+                            color: '#fff',
+                            width: 3,
+                          }),
+                        }),
                     }); 
                 }
             };
 
+            var view = new View({
+                projection: "EPSG:4326",
+                center: [0, 0],
+                zoom: 2,
+            });
+
             var vector = new VectorLayer({
               source: vectorSource,
               style: function (feature, resolution) {
-                return getStyle(feature, resolution);
+                return getStyle(feature, resolution, false);
                 }
             });
 
@@ -89,13 +112,34 @@ export default {
               ]),
               target: "map",
               layers: [raster, vector],
-              view: new View({
-                projection: "EPSG:4326",
-                center: [0, 0],
-                zoom: 2,
-              }),
+              view: view
             });
-        
+
+            //selection
+
+
+
+            var select = null
+
+            //select on single click
+            var selectSingleClick = new Select();
+
+            map.addInteraction(selectSingleClick);
+            selectSingleClick.on('select', function (e){
+              console.log("Something was selected")
+              console.log(e.selected[0].get("NAME"))
+              var feature = e.selected[0]
+              feature.setStyle(getStyle(feature, view.getResolution, true))
+              var polygon = feature.getGeometry()
+              view.fit(polygon, {padding: [30, 30, 30, 30]})
+            })
+
+            //select on pointermove
+
+            var selectPointerMove = new Select({
+              condition: pointerMove
+            });
+
      },
   },
 };
