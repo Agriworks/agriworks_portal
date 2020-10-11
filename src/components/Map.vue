af6afaa83e81a5acc1e15d0b93bc7315ff54b80e
Conor <cewalsh@bu.edu>
2020-10-10 23:29:09 -0400

    Made the color gradient dynamic

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -52,43 +52,19 @@ export default {
               features: new GeoJSON().readFeatures(geojsonObject),
             });
 
-            function getColor(d) {
-              return d > 1000 ? '#800026' :
-                    d > 900  ? '#BD0026' :
-                    d > 700  ? '#E31A1C' :
-                    d > 500  ? '#FC4E2A' :
-                    d > 300   ? '#FD8D3C' :
-                    d > 200   ? '#FEB24C' :
-                    d > 100   ? '#FED976' :
-                                '#FFEDA0';
-            }
-
-            function hexToRgb(hex) {
-              var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-              return result ? {
-                r: parseInt(result[1], 16),
-                g: parseInt(result[2], 16),
-                b: parseInt(result[3], 16)
-              } : null;
-            }
-
             var getStyle = function (feature, resolution) {
-                if (feature.get('data') < 10) {
-                    return new Style({
-                        fill: new Fill({
-                            color: [255, 0, 0, 0.2] // semi-transparent red
-                        })
-                    });
-                }
-                else {
-                    return new Style({
+                var color = feature.get('color')
+                if(color){
+
+                  return new Style({
                         stroke: new Stroke({
                           color: 'white',
                           lineDash: [1],
                           width: 1
                         }),
                         fill: new Fill({
-                            color: [hexToRgb(getColor(feature.get('data'))).r, hexToRgb(getColor(feature.get('data'))).g, hexToRgb(getColor(feature.get('data'))).b, 0.2]
+                            
+                            color: [color[0], color[1], color[2],  0.4]
                         })
                     }); 
                 }
