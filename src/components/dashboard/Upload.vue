611539da3cb0eb3aecd0d7f3df962be6f256952e
mjqPauli <mjqpauli@bu.edu>
2020-10-20 08:05:32 -0400

    delete original heatmap configuration code when uploading

--- src/components/dashboard/Upload.vue
+++ src/components/dashboard/Upload.vue
@@ -86,39 +86,6 @@
               
               <v-card-text>
                 <v-container>
-                  <!-- <v-row>
-                    <v-col cols="12" sm="6">
-                      <v-select
-                        v-model="heatmapData.longitude"
-                        :items="this.keys"
-                        label="Longitude"
-                        required
-                      ></v-select>
-                    </v-col>
-                    <v-col cols="12" sm="6">
-                      <v-select
-                        v-model="heatmapData.latitude"
-                        :items="this.keys"
-                        label="Latitude"
-                        required
-                      ></v-select>
-                    </v-col>
-                    <v-col cols="12">
-                      <v-select
-                        v-model="heatmapData.area"
-                        :items="this.keys"
-                        label="Area"
-                      ></v-select>
-                    </v-col>
-                    <v-col cols="12">
-                      <v-autocomplete
-                        v-model="heatmapData.value"
-                        :items="this.keys"
-                        label="Value"
-                        multiple
-                      ></v-autocomplete>
-                    </v-col>
-                  </v-row> -->
                   <v-stepper v-model="stepIndex">
                     <v-stepper-header>
                       <v-stepper-step
@@ -230,15 +197,6 @@
                   </v-stepper>
                 </v-container>
               </v-card-text>
-
-              <!-- <v-divider></v-divider> -->
-
-              <!-- <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
-                <v-btn color="blue darken-1" text @click="processForm">Save</v-btn>
-              </v-card-actions> -->
-
             </v-card>
           </v-dialog>
         </div>
@@ -271,12 +229,6 @@ export default {
       search: "",
       dialog: false,
       keys: [],
-      heatmapData: {
-        longitude: null,
-        latitude: null,
-        area: null,
-        value: null
-      },
       stepIndex: 1,
       hasTime: false,
       hasLocation: false,
